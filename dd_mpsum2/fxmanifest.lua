2b1d9fb5732c2cc785d780f60029010794846d9c
Quattro3498 <90252930+Quattro3498@users.noreply.github.com>
2022-08-31 00:45:07 +0200

    Add files via upload

---
+++ dd_mpsum2/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script {
+  'new_names.lua',
+}
+
+files {
+  'mpsum2/shop_tattoo.meta',
+  'mpsum2/mpsum2_overlays.xml',
+}
+
+data_file 'TATTOO_SHOP_DLC_FILE' 'mpsum2/shop_tattoo.meta'
+data_file 'PED_OVERLAY_FILE' 'mpsum2/mpsum2_overlays.xml'
---
+++ dd_mpsum2/readme.md
@@ -0,0 +1,2 @@
+Thank you for downloading the new "Criminal Enterprises" tattoo dlc.
+for more information and supportS visit our discord! https://discord.gg/aUsykSKtEz
